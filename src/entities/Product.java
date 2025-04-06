9300a910191b5f05b3a24a69ced9c626141cc946
fernandokolmar <fernandokolmar@gmail.com>
2025-04-06 13:25:16 +0100

    Adicionando a pasta src ao repositório

---
+++ src/entities/Product.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Product {
+    private String nome;
+    private double preco;
+
+    public Product(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
